1a08ed59c2befa6608bbba75c425807bf1cb6aab
ogasawarasatoru <world.wide.web.13@gmail.com>
2021-10-12 23:11:11 +0900

    make-userController

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Route::group(['namespace' => 'Api'], function () {
     Route::post('/line/callback', 'LineBotController@callback')->name('line.callback');
 });
