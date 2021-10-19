0b8c58455aba431532c2dbc6c590498e65eeac4c
ogasawarasatoru <world.wide.web.13@gmail.com>
2021-10-19 14:43:19 +0900

    add-user-deleteModal-blade

--- resources/views/pages/user/line_channel/channels_index.blade.php
+++ resources/views/pages/user/line_channel/channels_index.blade.php
@@ -27,7 +27,7 @@
                     <td>{{ $lineChannel->created_at->format('Y.m.d') }}</td>
                     <td>
                         <a class="btn btn btn-outline-success" href="{{ route('user.lineChannel.edit', $lineChannel->id) }}">編集</a>
-                        <a class="btn btn-outline-danger" href="{{ route('user.lineChannel.edit', $lineChannel->id) }}">削除</a>
+                        <a class="btn btn-outline-danger btn-delete" data-toggle="modal" data-target="#deleteModal" data-action-url="{{ route('user.lineChannel.destroy', $lineChannel->id) }}">削除</a>
                     </td>
                 </tr>
         </tbody>
@@ -39,13 +39,16 @@
 
     <!-- Modal -->
     <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="deleteModalLabel">削除しますか</h5>
+                    <h5 class="modal-title" id="deleteModalLabel">削除しますか?</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
+                </div>
+                <div class="modal-body">
+                    削除したチャンネルはLine Developer上には残ります。完全に削除する場合は、Line Developerのチャンネルも削除してください。
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">キャンセル</button>
@@ -59,7 +62,6 @@
         </div>
     </div>
 
-
 </div>
 
 @endsection
@@ -67,6 +69,7 @@
 @section('script')
 <script>
     $('.btn-delete').click(function (e) {
+        console.log($('#destroy-form').attr('action'));
         $('#destroy-form').attr('action', $(this).data('action-url'));
     });
 
