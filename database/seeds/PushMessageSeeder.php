91d7059ef91256e7af334e9c17795a668fc44c22
ogasawarasatoru <world.wide.web.13@gmail.com>
2021-10-24 01:18:30 +0900

    user-pushMessage-CRUD&&indexBlade

---
+++ database/seeds/PushMessageSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PushMessageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
