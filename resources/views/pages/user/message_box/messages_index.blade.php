59a3e262106ca272d7d1b319c21ad18a59fc7aa3
ogasawarasatoru <world.wide.web.13@gmail.com>
2021-10-25 23:51:18 +0900

    edit-user-channelMessageBlade

--- resources/views/pages/user/message_box/messages_index.blade.php
+++ resources/views/pages/user/message_box/messages_index.blade.php
@@ -1,23 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<h5 class="text-center">送信メッセージ一覧</h5>
+<h5 class="text-center">hogehoge送信メッセージ一覧</h5>
 
 <ul class="nav nav-tabs" id="myTab" role="tablist">
     <li class="nav-item">
-        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+        <a class="nav-link active" id="messageList-tab" data-toggle="tab" href="#messageList" role="tab" aria-controls="messageList" aria-selected="true">送信メッセージ</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+        <a class="nav-link" id="memberList-tab" data-toggle="tab" href="#memberList" role="tab" aria-controls="memberList" aria-selected="true">チェンネルメンバー</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+        <a class="nav-link" id="sendMessage-tab" data-toggle="tab" href="#sendMessage" role="tab" aria-controls="sendMessage" aria-selected="false">新規メッセージ作成</a>
     </li>
 </ul>
 <div class="tab-content" id="myTabContent">
-    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
-    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
-    <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+    <div class="tab-pane fade show active" id="messageList" role="tabpanel" aria-labelledby="messageList-tab">
+        <table class="table table-borderless table-hover mt-4">
+            <tbody>
+                <tr>
+                    <th scope="row">あががが</th>
+                    <td>ささささ</td>
+                    <td>2月13日</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="tab-pane fade" id="memberList" role="tabpanel" aria-labelledby="memberList-tab">...</div>
+    <div class="tab-pane fade" id="sendMessage" role="tabpanel" aria-labelledby="sendMessage-tab">...</div>
 </div>
 @endsection
 
