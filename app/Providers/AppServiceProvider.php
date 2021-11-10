248a814e5a3f39fadb5eecb86418dcc0867b614e
ogasawarasatoru <world.wide.web.13@gmail.com>
2021-11-11 02:58:11 +0900

    add-user-Api/PushMessageController

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use LINE\LINEBot;
 use App\Models\LineChannel;
+use LINE\LINEBot;
 
 class AppServiceProvider extends ServiceProvider
 {
